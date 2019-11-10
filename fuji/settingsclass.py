4fdd3a6743ab62f6811e3ad75283f328cba7071c
startechsheffield <57515980+startechsheffield@users.noreply.github.com>
2019-11-10 12:49:17 +0000

    Update and rename startech/settingsclass.py to fuji/settingsclass.py
    Added support for list data types, updated syntax errors, typos, and more.

--- fuji/settingsclass.py
+++ fuji/settingsclass.py
@@ -1,11 +1,23 @@
 from os.path import exists
-from startech.generalclass import checkToken, alignList
+from fuji.generalclass import checkToken, alignList
+def __checkstr__(val):
+	if not val.find("\n") < 0:
+		return(False)
+	badChars = "{|}"
+	for c in range(len(val)):
+		if not badChars.find(val[c]) < 0:
+			return(False)
+	return(True)
 def __convert__(val):
 	val = val.replace("\n","")
 	if val.lower() == "false":
 		return(False)
 	elif val.lower() == "true":
 		return(True)
+	elif val.endswith("}") == True:
+		val = val.replace("{","")
+		val = val.replace("}","")
+		return(val.split("|"))
 	try:
 		return(int(val))
 	except:
@@ -14,7 +26,7 @@ def getList(tkn):
 	if checkToken(tkn,True) == False:
 		return([])
 	try:
-		f = open("/usr/share/stech/api/reg_"+tkn+".txt","r")
+		f = open("/usr/share/fuji/api/reg_"+tkn+".txt","r")
 		lines = f.readlines()
 		f.close()
 	except:
@@ -25,39 +37,54 @@ def getList(tkn):
 			olist.append(lines[l].split(" : ")[0])
 	return(olist)
 def get(tkn,ttl,sf=False):
-	if checkToken(tkn,True) == False or not type(tkn) == str or not type(ttl) == str:
+	if checkToken(tkn,True) == False or checkToken(ttl,True) == False:
 		return([])
 	if not type(sf) == bool:
 		sf = False
 	try:
-		f = open("/usr/share/stech/api/reg_"+tkn+".txt","r")
+		f = open("/usr/share/fuji/api/reg_"+tkn+".txt","r")
 		lines = f.readlines()
 		f.close()
 	except:
 		return([])
 	for l in range(len(lines)):
 		if not lines[l].find("#") == 0 and lines[l].split(" : ")[0] == ttl:
-			if sf == False:
-				return(__convert__(lines[l].split(" : ")[1]))
-			return(lines[l].split(" : ")[1])
+			r = lines[l].split(" : ")[1]
+			if sf == False or r.endswith("}\n"):
+				r = __convert__(r)
+				if not type(r) == list or sf == True:
+					return(r)
+				for v in range(len(r)):
+					r[v] == __convert__(r[v])
+			return(r)
 	return("")
 def set(tkn,ttl,val):
-	if checkToken(tkn,True) == False or not type(ttl) == str:
+	if checkToken(tkn,True) == False or checkToken(ttl,True) == False:
 		return(False)
-	if not (type(val) == str or type(val) == int or type(val) == bool):
+	if type(val) == list:
+		ostr = "{"
+		for e in range(len(val)):
+			if (type(val[e]) == str and __checkstr__(val[e]) == False) and not (type(val) == int or type(val) == bool):
+				return(False)
+			if not e == 0:
+				ostr = ostr + "|"
+			ostr = ostr + str(val[e])
+		ostr = ostr + "}"
+		val = ostr
+	elif not (type(val) == str or type(val) == int or type(val) == bool):
 		return(False)
-	if exists("/usr/share/stech/api/reg_"+tkn+".txt") == False:
+	print(val)
+	if exists("/usr/share/fuji/api/reg_"+tkn+".txt") == False:
 		try:
-			f = open("/usr/share/stech/api/reg_"+tkn+".txt","w")
+			f = open("/usr/share/fuji/api/reg_"+tkn+".txt","w")
 			f.write("# Please try to avoid editing this file manually if possible.\n")
-			f.write("# 'stech-manager --config NAME=VALUE' edits these files.\n")
 			f.write(ttl+" : "+str(val)+"\n")
 			f.close()
 			return(True)
 		except:
 			return(False)
 	try:
-		f = open("/usr/share/stech/api/reg_"+tkn+".txt","r")
+		f = open("/usr/share/fuji/api/reg_"+tkn+".txt","r")
 		lines = f.readlines()
 		f.close()
 	except:
@@ -71,17 +98,17 @@ def set(tkn,ttl,val):
 	if found == False:
 		lines.append(ttl+" : "+str(val)+"\n")
 	try:
-		f = open("/usr/share/stech/api/reg_"+tkn+".txt","w")
+		f = open("/usr/share/fuji/api/reg_"+tkn+".txt","w")
 		f.writelines(lines)
 		f.close()
 	except:
 		return(False)
 	return(True)
 def unset(tkn,ttl):
-	if checkToken(tkn,True) == False or not type(ttl) == str:
+	if checkToken(tkn,True) == False or checkToken(ttl,True):
 		return(False)
 	try:
-		f = open("/usr/share/stech/api/reg_"+tkn+".txt","r")
+		f = open("/usr/share/fuji/api/reg_"+tkn+".txt","r")
 		lines = f.readlines()
 		f.close()
 	except:
@@ -92,7 +119,7 @@ def unset(tkn,ttl):
 			break
 	lines = alignList(lines)
 	try:
-		f = open("/usr/share/stech/api/reg_"+tkn+".txt","w")
+		f = open("/usr/share/fuji/api/reg_"+tkn+".txt","w")
 		f.writelines(lines)
 		f.close()
 		return(True)
