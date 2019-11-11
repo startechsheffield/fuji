5af40c091f8d8e43d5a064fd9caf68ae6507b0ec
startechsheffield <57515980+startechsheffield@users.noreply.github.com>
2019-11-11 23:22:41 +0000

    Update generalclass.py
    Migrated list-related functions to new class

--- fuji/generalclass.py
+++ fuji/generalclass.py
@@ -61,41 +61,6 @@ def checkToken(tkn,ibl=False):
 	return(False)
 def checkRoot():
 	return(geteuid() == 0)
-def combineLists(l1,l2):
-	if not type(l1) == list or not type(l2) == list:
-		return([])
-	for a in range(len(l2)):
-		found = False
-		for b in range(len(l1)):
-			if type(l2[a]) == type(l1[b]) and l2[a] == l1[b]:
-				found = True
-				break
-		if found == False:
-			l1.append(l2[a])
-	return(l1)
-def alignList(li):
-	if not type(li) == list:
-		return([])
-	olist = []
-	for i in range(len(li)):
-		if type(li[i]) == str:
-			if not li[i] == "":
-				olist.append(li[i])
-		elif type(li[i]) == list:
-			if not li[i] == []:
-				olist.append(li[i])
-		else:
-			olist.append(li[i])
-	return(olist)
-def insertList(li,val,pos):
-	if not type(li) == list or not type(pos) == int or 0 > pos > len(li):
-		return([])
-	olist = []
-	for l in range(len(li)):
-		if l == pos:
-			olist.append(val)
-		olist.append(li[l])
-	return(olist)
 def pythonVersion():
 	v = version.split(" ")[0].split(".")
 	return(v[0]+"."+v[1])
